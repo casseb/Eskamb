4f2653125347afa0f8555ef5417d35a9bb057a8b
By Casseb <felipe.casseb@gmail.com>
2016-04-28 20:16:26 -0300

    Adaptado o template, corrigido bugs no menu e iniciado o conceito completo de extends

--- Eskamb_App/views.py
+++ Eskamb_App/views.py
@@ -3,7 +3,7 @@ from django.http.response import HttpResponse
 from django.template import RequestContext, loader
 
 def home(request):
-    template = loader.get_template('menu.html')
+    template = loader.get_template('home.html')
     return HttpResponse(template.render())
 
 
