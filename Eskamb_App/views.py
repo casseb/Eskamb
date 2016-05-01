08f626b23ace9be59c816f40d4962f6524fbcc8e
By Casseb <felipe.casseb@gmail.com>
2016-05-01 00:05:02 -0300

    Concluido template e home (visual)

--- Eskamb_App/views.py
+++ Eskamb_App/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template import RequestContext, loader
+# -*- coding: utf-8 -*-
 
 def home(request):
-    template = loader.get_template('home.html')
+    template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
 
