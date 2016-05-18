b1833eee733da3351b5f993f657e5979aff5688d
By Casseb <felipe.casseb@gmail.com>
2016-05-18 00:37:12 -0300

    Concluido Objeto Categoria

--- Eskamb_App/admin.py
+++ Eskamb_App/admin.py
@@ -1,3 +1,4 @@
+from Eskamb_App.models import *
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Categoria)
--- Eskamb_App/models.py
+++ Eskamb_App/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length = 100)
+    aprovado = models.CharField(max_length = 1)
+    
+    def __str__(self):
+        return self.nome
